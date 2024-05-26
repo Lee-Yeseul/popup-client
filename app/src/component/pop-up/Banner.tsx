166f793ab32a82b39c9a2482400aa57eff612f64
YESEUL LEE <95066223+Lee-Yeseul@users.noreply.github.com>
2024-05-26 22:54:54 +0900

    [KAN-1] banner image 추가, footer 링크 연결

--- app/src/component/pop-up/Banner.tsx
+++ app/src/component/pop-up/Banner.tsx
@@ -1,7 +1,11 @@
+import ImageBox from '../common/ImageBox'
+
 export default function Banner() {
   return (
     <div className="w-full">
-      <div className="bg-primary-300 h-56 w-full"></div>
+      <div className="relative h-64 w-full bg-gray-300">
+        <ImageBox imagePath="banner/banner_1.png" />
+      </div>
     </div>
   )
 }
