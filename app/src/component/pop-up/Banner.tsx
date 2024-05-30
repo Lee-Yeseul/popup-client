203654599178e55a3292d1ef108a1c1e5e78f240
YESEUL LEE <95066223+Lee-Yeseul@users.noreply.github.com>
2024-05-30 16:22:28 +0900

    feature : banner image 설정 수정

--- app/src/component/pop-up/Banner.tsx
+++ app/src/component/pop-up/Banner.tsx
@@ -4,7 +4,11 @@ export default function Banner() {
   return (
     <div className="w-full">
       <div className="relative h-64 w-full bg-gray-300">
-        <ImageBox imagePath="banner/banner_1.png" />
+        <ImageBox
+          imagePath="banner/banner_1.png"
+          alt="banner"
+          objectFit="object-fill"
+        />
       </div>
     </div>
   )
